43521922a0679949932b0ffeedb39239d433fd6a
Bartosz Górka <bartosz.gorka@interia.eu>
2019-01-12 19:50:42 +0100

    Find best shift time and min value of penalties

--- scheduler.py
+++ scheduler.py
@@ -150,6 +150,33 @@ class Scheduler:
         value = self.calculate_penalties(ordered, time)
         return ordered, value, time
 
+    def find_best_shift(self, tasks):
+        best_start_time = 0
+
+        # We should always start in Time = 0 to check one of possibled optional point
+        # Next we will move x time units to ensure task finish in due date line
+        start_time = 0
+        min_value = self.calculate_penalties(tasks, start_time)
+
+        # Now we should move tasks to finish in due date line
+        last_task_index = None
+        for index, task in enumerate(tasks):
+            start_time += task['p']
+            if start_time >= self.due_date:
+                start_time -= (start_time - task['p'])
+                last_task_index = index - 1
+                break
+
+        # Now in loop we can shift time and verify penalties
+        for index in range(last_task_index, -1, -1):
+            value = self.calculate_penalties(tasks, start_time)
+            if value < min_value:
+                min_value = value
+                best_start_time = start_time
+            start_time += tasks[index]['p']
+
+        return min_value, best_start_time
+
 
     def shedule_shift_and_verify(self):
         min_value = sys.maxsize
