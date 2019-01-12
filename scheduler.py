ec4cd6019d93b482406a307461f5f5681fe3e6cc
Bartosz Górka <bartosz.gorka@interia.eu>
2019-01-12 19:09:57 +0100

    Scheduler with option split tasks 50/50 by processing time

--- scheduler.py
+++ scheduler.py
@@ -100,6 +100,56 @@ class Scheduler:
         value = self.calculate_penalties(ordered, time)
         return ordered, value, time
 
+    def schedule_from_due_date_time(self):
+        """Split 50-50 by time (processing time) and schedule"""
+        # Extra variables with lists - to store calculations
+        ordered = []
+        used_tasks = []
+
+        # Sort task on list
+        sorted_tasks = self.sort_before_due_date()
+
+        # We want split it 50-50 - first part before, second after due date
+        # Reversed also to start from due date
+        first_part = []
+        sum_of_time = 0
+        all_task_processing_half_time = self.tasks_processing_time / 2
+        for task in sorted_tasks:
+            sum_of_time += task['p']
+            if sum_of_time > self.due_date or sum_of_time > all_task_processing_half_time:
+                # We should finish append action without this task
+                break
+            else:
+                first_part.append(task)
+        first_part.reverse()
+
+        # Start in due date
+        time = self.due_date
+
+        # Iterations over tasks
+        for index, task in enumerate(first_part):
+            time -= task['p']
+            if time < 0:
+                time -= task['p']
+                break
+
+            ordered.append(task)
+            used_tasks.append(index)
+
+        # Reverse tasks because started from due date
+        ordered.reverse()
+
+        # Remove used tasks from list and sort it
+        tasks_to_schedule = delete(sorted_tasks, used_tasks)
+        sorted_tasks = self.sort_after_due_date(tasks_to_schedule)
+
+        # Order tasks after due date
+        [ordered.append(task) for task in sorted_tasks]
+
+        # Calculate goal function value
+        value = self.calculate_penalties(ordered, time)
+        return ordered, value, time
+
 
     def shedule_shift_and_verify(self):
         min_value = sys.maxsize
