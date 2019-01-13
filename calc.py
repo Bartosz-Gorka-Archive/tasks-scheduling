693b1055e68b5f2afb9e3f4f44d90cfcfc7b285b
Bartosz Górka <bartosz.gorka@interia.eu>
2019-01-13 07:34:03 +0100

    Remove required parameters in Calc module

--- calc.py
+++ calc.py
@@ -2,32 +2,26 @@ from math import floor
 
 
 class Calc():
-    def __init__(self, tasks, h):
-        self.h = h
-        self.tasks = tasks.copy()
-        self.tasks_processing_time = None
-        self.due_date = None
+    def calculate_sum_times(self, tasks):
+        return sum([task['p'] for task in tasks])
 
-    def calculate_sum_times(self):
-        self.tasks_processing_time = sum([task['p'] for task in self.tasks])
+    def calculate_due_date(self, tasks_processing_time, h):
+        return int(floor(tasks_processing_time * h))
 
-    def calculate_due_date(self):
-        self.due_date = int(floor(self.tasks_processing_time * self.h))
-
-    def calculate_penalties(self, tasks, start):
+    @staticmethod
+    def calculate_penalties(tasks, start, due_date):
         result = 0
         time = start
 
         for task in tasks:
-            result += task['a'] * max(self.due_date - (task['p'] + time), 0) \
-                      + task['b'] * max((task['p'] + time) - self.due_date, 0)
+            result += task['a'] * max(due_date - (task['p'] + time), 0) \
+                      + task['b'] * max((task['p'] + time) - due_date, 0)
             time += task['p']
 
         return result
 
-    def setup(self):
-        self.calculate_sum_times()
-        self.calculate_due_date()
+    def setup(self, tasks, h):
+        tasks_processing_time = self.calculate_sum_times(tasks=tasks)
+        due_date = self.calculate_due_date(tasks_processing_time=tasks_processing_time, h=h)
 
-    def parameters(self):
-        return self.tasks_processing_time, self.due_date
+        return tasks_processing_time, due_date
